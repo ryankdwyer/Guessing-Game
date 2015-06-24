9a8f8bdb049c01eb2544c2069147a9282bae88af
Ryan Dwyer <ryankdwyer@gmail.com>
2015-06-23 22:42:14 -0400

    Added app.js file

---
+++ app.js
@@ -0,0 +1,8 @@
+// Generate random number 1-100 - store in variable
+
+// User enters guess in input
+	// Test whether this value is correct
+		// If correct
+			// Do something cool!
+		// else
+			// tell user if they are hot or cold
