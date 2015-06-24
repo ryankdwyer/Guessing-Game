3877f8627a508855e0ab0c1a6dbe92f0b69e21b5
Ryan Dwyer <ryankdwyer@gmail.com>
2015-06-24 08:23:49 -0400

    Added app.js file

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // Generate random number 1-100 - store in variable
+// Initialize an array to hold guesses
+// Initialize variable to count number of guesses
 
-// User enters guess in input
+
+// User enters guess in input and clicks submit
+	// Validate the input is a number and between 1-100
+	// Validate the input is not a repeat
+	// Increment number of guesses - check if it exceeds limit
 	// Test whether this value is correct
 		// If correct
 			// Do something cool!
 		// else
-			// tell user if they are hot or cold
+			// tell user if they are hot or cold by updating a <p> - tell
+			// tell the user if they need to guess higher or lower
+
+// User clicks reset button
+	// clear variables saving game state
+	// generate new random number
+	// redisplay opening message
+
+// User clicks hint
+	// tell user the number is between -5 & +5 of the answer
+	// Count it as a guess?
