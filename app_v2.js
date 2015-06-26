b535d9329dd68a7a47a5f11a5d1a5fa5ce118d45
Ryan Dwyer <ryankdwyer@gmail.com>
2015-06-25 23:01:22 -0400

    Made changes to app_v2.js, html, and css

--- app_v2.js
+++ app_v2.js
@@ -11,62 +11,98 @@ var numGuesses = guesses.length;
 var guessLimit = 10;
 // Initialize guess variable
 var currentGuess;
+// Change number of guesses div text
+$("#guessesLeft").text("You have " + (guessLimit - numGuesses) + " guesses left.")
+// focus on input
+$("#guess").focus();
+// Add 1 - 100 div tags hide all
+var $ul = $('<ul id="Container"></ul>');
+var $ul2 = $('<ul id="Container"></ul>');
+for (var i=1; i < 51; i++) {
+	$ul.append($('<li id=' + i + ' class="numString">' + i + '</li>'));
+};
+for (var i=51; i < 101; i++) {
+	$ul2.append($('<li id=' + i + ' class="numString">' + i + '</li>'));
+};
+$("#input").append($ul);
+$("#input").append($ul2);
 
-
-// Validate the input is a number and between 1-100
-// function isNumber(currentGuess) {
-// 	if (typeof currentGuess === 'number') {
-// 		return true;
-// 	} else {
-// 		alert("That entry is not a number. Please enter a number between 1 and 100.")
-// 		return false;
-// 	}
-// }
 // Validate the input is not a repeat
 function newGuess(inputGuess) {
 	if ($.inArray(inputGuess, guesses) === -1) {
 		return true;		
 	} else {
 		alert("It looks like you already guessed that number.")
-		$("#guess").val('');
+		$("#guess").val('');	
 	}
 }
 
 // Resets game
 function reset() {
-	alert("Sorry, you are out of guesses");
 	answer = randomNumberGenerator(1,100);
 	guesses = [];
+	numGuesses = guesses.length;
+	$("#guessesLeft").text("You have " + (guessLimit - numGuesses) + " guesses left.");
 	$("#guess").val('');
+	$("#guess").focus();
+	$("#feedback").text("Guess a number from 1 to 100");
+}
+
+// Creates response decisions
+function responses() {
+	var guess = Number(currentGuess);
+	// If guess is greater than 20 numbers away
+	if (guess > answer) {
+		if ((guess - answer) > 20) {
+			$("#guess").val('');
+			$("#feedback").text("Guess lower");
+		} else if (10 <= (guess - answer) < 20) {
+			$("#guess").val('');
+			$("#feedback").text("You're getting close! Guess lower.");
+		} else {
+			$("#feedback").text("You're REALLY close! Guess lower.");
+		}
+	} else {
+		if ((answer - guess) > 20) {
+			$("#guess").val('');
+			$("#feedback").text("Guess higher");
+		} else if (10 <= (answer - guess) < 20) {
+			$("#guess").val('');
+			$("#feedback").text("You're getting close! Guess higher.");
+		} else {
+			$("#feedback").text("You're REALLY close! Guess higher.");
+		}
+	}
+	$("#guess").focus();
 }
 
 // User enters guess in input and clicks submit
 $("#submit").click(function () {
 	currentGuess = $("#guess").val();
 	// currentGuess = Number($("#guess").val());
-	if ($.isNumeric(currentGuess) &&) {
-		alert("that is a number");
-		if (newGuess(Number(currentGuess))) {
-			alert('thats a new guess!');
-			if (Number(currentGuess) === answer) {
-			// If correct
-			$("#feedback").text("yay you win!");
+	if ($.isNumeric(currentGuess) && newGuess(Number(currentGuess)) && (1 <= Number(currentGuess) <= 100)) {
+
+		if (Number(currentGuess) === answer) {
+		// If correct
+		$("#feedback").text("yay you win!");
+		} else {
+			// append to guesses
+			guesses.push(Number(currentGuess));
+			// update numGuesses
+			numGuesses = guesses.length;
+			$("#guessesLeft").text("You have " + (guessLimit - numGuesses) + " guesses left.");
+			// check if it exceeds limit
+			if (numGuesses >= guessLimit) {
+				alert("Sorry, you are out of guesses.");
+				reset();
 			} else {
-				// append to guesses
-				guesses.push(Number(currentGuess));
-				// check if it exceeds limit
-				if (numGuesses >= guessLimit) {
-					reset();
-				} else {
-					// tell user if they are hot or cold by updating a <p> - tell
-					// tell the user if they need to guess higher or lower
-				}
+				responses();
 			}
+		}
 		// Add guess to the number line
 		// Change class of div to 'seleceted'
 			// This will show the number on the number line of guesses
 		// Test whether this value is correct
-		} 	
 	} else {
 		$("#feedback").text("That is not a number!");
 		$("#guess").val('');
@@ -75,16 +111,26 @@ $("#submit").click(function () {
 
 // User clicks reset button
 $("#reset").click(function (){
+	// require user confirmation of game reset through a prompt
 	if (confirm("Are you sure you want to reset the game?")) {
 		reset();
 	}
 });
-	// require user confirmation of game reset through a prompt
-	// clear variables saving game state
-	// generate new random number
-	// redisplay opening message
-
+	
 // User clicks hint
-	// tell user the number is between -5 & +5 of the answer
-	// Count it as a guess?
+$("#hint").click(function (){
+	if (confirm('Are you sure you want a hint? It will cost you a guess!')) {
+		if (answer < 10) {
+			var lower = 0;
+			var higher = answer + 10;
+		} else if (answer > 90) {
+			var lower = answer - 10;
+			var higher = 100;
+		} else {
+			var lower = answer - 10;
+			var higher = answer + 10;
+		};
+		alert("The answer is between " + lower + " and " + higher + ".");
+	}
+});
 
